07e97f127d688e61a1e0f86e9d8b04ff2b060f12
nicklayb <nicklay@me.com>
2025-11-06 22:23:33 -0500

    Improve ui

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -2,35 +2,46 @@ module Page.Home exposing (Model, Msg, init, toSession, update, view)
 
 import Dict
 import GitHub
-import Html exposing (Html, a, dd, div, dl, dt, h1, span, text)
+import Html exposing (Html, a, div, h1, li, span, text, ul)
 import Html.Attributes exposing (class, href)
 import Html.Events exposing (onClick)
-import Json.Decode
-import Plan exposing (Plan)
 import RemotePlan exposing (RemotePlan)
 import Resource exposing (Resource(..))
 import Route exposing (Route)
 import Session exposing (Session)
+import Set exposing (Set)
 import Tree exposing (Folder(..))
 
 
 type alias Model =
     { session : Session
     , indexedPlans : Resource (Folder RemotePlan)
+    , openedFolders : Set String
     }
 
 
 type Msg
     = IndexFetched (GitHub.HttpResponse (Folder RemotePlan))
+    | ToggleFolder String
 
 
 init : Session -> ( Model, Cmd Msg )
 init session =
     ( { session = session
       , indexedPlans = Loading Nothing
+      , openedFolders = Set.insert "/" Set.empty
       }
     , GitHub.fetchIndex IndexFetched
     )
+
+
+toggleSet : comparable -> Set comparable -> Set comparable
+toggleSet item set =
+    if Set.member item set then
+        Set.remove item set
+
+    else
+        Set.insert item set
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -38,6 +49,9 @@ update msg model =
     case msg of
         IndexFetched (Ok plans) ->
             ( { model | indexedPlans = Loaded plans }, Cmd.none )
+
+        ToggleFolder folderKey ->
+            ( { model | openedFolders = toggleSet folderKey model.openedFolders }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
@@ -48,28 +62,44 @@ toSession { session } =
     session
 
 
-viewPlans : String -> Folder RemotePlan -> Model -> Html Msg
-viewPlans title (Folder plans) model =
+viewPlans : ( String, String ) -> Folder RemotePlan -> Model -> Html Msg
+viewPlans ( folderKey, title ) (Folder plans) model =
     let
         viewFiles =
             Dict.foldl viewFile []
 
         viewFile _ file acc =
-            a [ href <| Route.routeToString (Route.RunPlan (Just file.path) Nothing) ] [ text file.description ] :: acc
+            a [ class "border border-gray-300 rounded-sm px-2 py-0.5 text-pink-500 hover:text-pink-600 mb-2", href <| Route.routeToString (Route.RunPlan (Just file.path) Nothing) ] [ text file.description ] :: acc
 
         viewFolders =
             Dict.foldl viewFolder []
 
         viewFolder key folder acc =
-            viewPlans key folder model :: acc
+            viewPlans ( folderKey ++ "_" ++ key, key ) folder model :: acc
+
+        isToggled =
+            Set.member folderKey model.openedFolders
+
+        ( icon, maybeViewContent ) =
+            if isToggled then
+                ( "-"
+                , [ li [ class "pl-4" ]
+                        (viewFiles plans.files
+                            ++ viewFolders plans.folders
+                        )
+                  ]
+                )
+
+            else
+                ( "+", [] )
     in
-    dl []
-        [ dt [] [ text title ]
-        , dd [ class "pl-4" ]
-            (viewFiles plans.files
-                ++ viewFolders plans.folders
-            )
-        ]
+    ul [ class "border border-gray-200 rounded-md p-2" ]
+        (li [ class "mb-2 cursor-pointer hover:text-pink-700", onClick (ToggleFolder folderKey) ]
+            [ span [ class "text-xl mr-2" ] [ text icon ]
+            , span [] [ text title ]
+            ]
+            :: maybeViewContent
+        )
 
 
 viewContent : Model -> Html Msg
@@ -78,7 +108,7 @@ viewContent model =
         innerContent =
             case model.indexedPlans of
                 Loaded plans ->
-                    viewPlans "/" plans model
+                    div [ class "max-w-2xl w-full" ] [ viewPlans ( "/", "/" ) plans model ]
 
                 _ ->
                     div [] [ text "Loading..." ]
